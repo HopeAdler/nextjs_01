47047a859f27d5c93551b20409257ab5adfd7d8b
HopeAdler <buihoangphucthd@gmail.com>
2024-09-17 13:20:31 +0700

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>Hello World NextJS
+      <div>
+        <Button type="primary">Add New</Button>
+      </div>
+    </div>
+  );
+}
